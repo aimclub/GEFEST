241c8d6db3ac551df4db9e12ec5884ef8fe908c2
quickjkee <jkee2013@mail.ru>
2021-11-12 22:59:46 +0300

    init

--- cases/synthetic/circle.py
+++ cases/synthetic/circle.py
@@ -3,6 +3,7 @@ from gefest.core.opt.optimize import optimize
 from gefest.core.opt.setup import Setup
 from gefest.core.structure.domain import Domain
 from gefest.core.structure.structure import Structure
+import numpy as np
 
 geometry = Geometry2D()
 domain = Domain(allowed_area=[(0, 0),
@@ -23,7 +24,7 @@ def area_length_ratio(struct: Structure):
     if area == 0:
         return None
 
-    return length / area
+    return (1  - 4*np.pi*area / length**2)
 
 
 optimized_structure = optimize(task_setup=task_setup,
