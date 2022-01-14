a3d7b0c3861f268f6ec9ad90f5fe548b4768af18
Denis Sidorenko <70846761+DenisSidoren@users.noreply.github.com>
2022-01-14 16:08:44 +0300

    Add files via upload

---
+++ test/test_geometry2D.py
@@ -0,0 +1,29 @@
+# import sys
+# sys.path.append('/Users/denis/OneDrive - ITMO UNIVERSITY/NSS_work/GEFEST')
+
+import pytest
+from gefest.core.geometry.geometry_2d import Geometry2D
+from gefest.core.structure.domain import Domain
+
+
+
+def test_Geometry2d():
+    geometry = Geometry2D()
+    domain = Domain()
+
+    def resize_poly_test():
+        poly = domain.bound_poly()
+        x_scale = 1.5
+        y_scale = 1.5
+        new_poly = geometry.resize_poly(poly, x_scale= x_scale,
+                                              y_scale= y_scale)
+        square_difference = geometry.get_square(new_poly) - geometry.get_square(poly)
+
+        assert square_difference != 0
+
+
+
+
+
+
+
