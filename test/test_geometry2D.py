2a7b4782f8c223b6e5e520ee98d424f2572d8d20
Denis Sidorenko <sidorenko.den95@gmail.com>
2022-01-19 16:39:46 +0300

    fixed PEP8 issues

--- test/test_geometry2D.py
+++ test/test_geometry2D.py
@@ -6,18 +6,17 @@ from gefest.core.structure.domain import Domain
 geometry = Geometry2D()
 domain = Domain()
 
+
 def test_resize_poly():
     a = b = 10
     x_scale = 1.5
     y_scale = 1.5
-    domain = Domain(allowed_area = [(0, 0),
-                                    (0, b),
-                                    (a, b),
-                                    (a, 0)])
+    domain = Domain(allowed_area = [(0, 0), (0, b), (a, b), (a, 0)])
     poly = domain.bound_poly()
-    new_poly = geometry.resize_poly(poly, x_scale= x_scale,
-                                          y_scale= y_scale)
-    square_difference = geometry.get_square(new_poly) - geometry.get_square(poly)
+    new_poly = geometry.resize_poly(poly, x_scale= x_scale, y_scale= y_scale)
+    square_newpoly = geometry.get_square(new_poly)
+    square_poly = geometry.get_square(poly)
+    square_difference = square_newpoly - square_poly
     square_hands_calc = ((a*x_scale) * (b*y_scale)) - a*b
 
     assert square_difference == square_hands_calc
