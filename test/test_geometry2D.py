0857c16857c26d87b6094cc0ce8987f329904905
Denis Sidorenko <sidorenko.den95@gmail.com>
2022-01-20 10:34:26 +0300

    made test harder and renamed variables

--- test/test_geometry2D.py
+++ test/test_geometry2D.py
@@ -1,22 +1,28 @@
 import pytest
+import numpy as np
+
 from gefest.core.geometry.geometry_2d import Geometry2D
 from gefest.core.structure.domain import Domain
-
 
 geometry = Geometry2D()
 domain = Domain()
 
 
 def test_resize_poly():
-    a = b = 10
-    x_scale = 1.5
-    y_scale = 1.5
-    domain = Domain(allowed_area = [(0, 0), (0, b), (a, b), (a, 0)])
-    poly = domain.bound_poly()
-    new_poly = geometry.resize_poly(poly, x_scale= x_scale, y_scale= y_scale)
-    square_newpoly = geometry.get_square(new_poly)
-    square_poly = geometry.get_square(poly)
-    square_difference = square_newpoly - square_poly
-    square_hands_calc = ((a*x_scale) * (b*y_scale)) - a*b
+    a = np.random.uniform(0,100)
+    b = np.random.uniform(0,100)
+    x_scale = np.random.uniform(0,10)
+    y_scale = np.random.uniform(0,10)
 
-    assert square_difference == square_hands_calc
+    domain = Domain(allowed_area = [(0, 0), (0, b), (a, b), (a, 0)])
+
+    original_poly = domain.bound_poly()
+    resized_poly = geometry.resize_poly(original_poly, x_scale = x_scale, y_scale = y_scale)
+
+    resized_square = geometry.get_square(resized_poly)
+    original_square = geometry.get_square(original_poly)
+
+    observed_difference = resized_square - original_square
+    expected_difference = ((a*x_scale) * (b*y_scale)) - a*b
+
+    assert np.isclose(observed_difference, expected_difference) == True
