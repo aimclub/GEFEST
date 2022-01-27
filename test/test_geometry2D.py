65f6d70240883b26ee72fd8699c38b31af7557c0
Denis Sidorenko <sidorenko.den95@gmail.com>
2022-01-27 18:43:44 +0300

    create test for rotate function

--- test/test_geometry2D.py
+++ test/test_geometry2D.py
@@ -5,19 +5,17 @@ from gefest.core.geometry.geometry_2d import Geometry2D
 from gefest.core.structure.domain import Domain
 
 geometry = Geometry2D()
-domain = Domain()
+a = 5
+b = 10
+domain = Domain(allowed_area=[(0, 0), (0, b), (a, b), (a, 0)])
 
 
 def test_resize_poly():
-    a = np.random.uniform(0,100)
-    b = np.random.uniform(0,100)
-    x_scale = np.random.uniform(0,10)
-    y_scale = np.random.uniform(0,10)
+    x_scale = 2
+    y_scale = 3
 
-    domain = Domain(allowed_area = [(0, 0), (0, b), (a, b), (a, 0)])
-
-    original_poly = domain.bound_poly()
-    resized_poly = geometry.resize_poly(original_poly, x_scale = x_scale, y_scale = y_scale)
+    original_poly = domain.bound_poly
+    resized_poly = geometry.resize_poly(original_poly, x_scale=x_scale, y_scale=y_scale)
 
     resized_square = geometry.get_square(resized_poly)
     original_square = geometry.get_square(original_poly)
@@ -25,4 +23,16 @@ def test_resize_poly():
     observed_difference = resized_square - original_square
     expected_difference = ((a*x_scale) * (b*y_scale)) - a*b
 
-    assert np.isclose(observed_difference, expected_difference) == True
+    assert np.isclose(observed_difference, expected_difference)
+
+
+def test_rotate_poly():
+    angle = 90
+
+    original_poly = domain.bound_poly
+    rotate_poly = geometry.rotate_poly(original_poly, angle=angle)
+
+    expected_domain = Domain(allowed_area=[(-2.5, 2.5), (-2.5, 7.5), (7.5, 7.5), (7.5, 2.5)])
+    expected_poly = expected_domain.bound_poly
+
+    assert expected_poly == rotate_poly
