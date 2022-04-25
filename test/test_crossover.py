88425c89174b26d89cad093a17bd340d5e872c64
Denis Sidorenko <sidorenko.den95@gmail.com>
2022-04-25 15:24:58 +0300

    crossover test stg2

--- test/test_crossover.py
+++ test/test_crossover.py
@@ -1,49 +1,27 @@
 import pytest
-from copy import deepcopy
 from gefest.core.opt.operators.crossover import crossover
 from gefest.core.structure.point import Point
 from gefest.core.structure.polygon import Polygon
-from gefest.core.structure.structure import get_random_structure
 from gefest.core.structure.structure import Structure
 from gefest.core.structure.domain import Domain
 
-
-def get_structure_points(structure: Structure):
-    temp = []
-    for poly in structure.polygons:
-        temp.append([(pt.x, pt.y) for pt in poly.points])
-    return temp
+def create_rectangle(x,y):
+    rectangle_points = [(x, y), (x, y+5), (x+5, y+5), (x+5, y), (x,y)]
+    rectangle_poly = Polygon(f'rectangle from {x,y}', points=[Point(*coords) for coords in rectangle_points])
+    return rectangle_poly
 
 
 def test_crossover_true():
-    '''testing when crossover will be passed'''
 
-    domain = Domain(min_poly_num=2,
-                    max_poly_num=4)
-    structure_1 = get_random_structure(domain)
-    structure_2 = get_random_structure(domain)
+    domain = Domain()
+    structure_1 = Structure([create_rectangle(5,5), create_rectangle(5,15)])
+    structure_2 = Structure([create_rectangle(15,5), create_rectangle(15,15)])
 
-    new_structure = crossover(structure_1, structure_2, domain, rate=0.99)
+    expected_structure = [structure_1.polygons[0].points, structure_2.polygons[1].points]
 
-    all_new_points = get_structure_points(new_structure)
-    all_points_structure_1 = get_structure_points(structure_1)
-    all_points_structure_2 = get_structure_points(structure_2)
-    print(all_points_structure_1)
-    print(all_points_structure_2)
-
-    assert any([True for s1_pt in all_points_structure_1 if s1_pt in all_new_points])
-    assert any([True for s2_pt in all_points_structure_2 if s2_pt in all_new_points])
-
-
-# def test_crossover_false():
-#     '''testing when crossover will NOT be passed'''
-
-#     domain = Domain(min_poly_num=2,
-#                     max_poly_num=4)
-#     structure_1 = get_random_structure(domain)
-#     structure_2 = get_random_structure(domain)
-
-#     new_structure = crossover(structure_1, structure_2, domain, rate=0.01)
-
-#     assert  all([s1==new for s1, new in zip(structure_1.polygons, new_structure.polygons)])
-#     assert not all([s2==new for s2, new in zip(structure_2.polygons, new_structure.polygons)])
+    for i in range(100):
+        new_structure = crossover(structure_1, structure_2, domain)
+        if all([new_structure.polygons[0].points == expected_structure[0],
+                new_structure.polygons[1].points == expected_structure[1]]):
+                assert True
+                
