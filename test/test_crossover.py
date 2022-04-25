11b84840b14f9f31d7da9d7c067867f75d2bc9b8
Denis Sidorenko <sidorenko.den95@gmail.com>
2022-04-25 15:27:46 +0300

    pep8

--- test/test_crossover.py
+++ test/test_crossover.py
@@ -5,8 +5,9 @@ from gefest.core.structure.polygon import Polygon
 from gefest.core.structure.structure import Structure
 from gefest.core.structure.domain import Domain
 
-def create_rectangle(x,y):
-    rectangle_points = [(x, y), (x, y+5), (x+5, y+5), (x+5, y), (x,y)]
+
+def create_rectangle(x, y):
+    rectangle_points = [(x, y), (x, y+5), (x+5, y+5), (x+5, y), (x, y)]
     rectangle_poly = Polygon(f'rectangle from {x,y}', points=[Point(*coords) for coords in rectangle_points])
     return rectangle_poly
 
@@ -14,8 +15,8 @@ def create_rectangle(x,y):
 def test_crossover_true():
 
     domain = Domain()
-    structure_1 = Structure([create_rectangle(5,5), create_rectangle(5,15)])
-    structure_2 = Structure([create_rectangle(15,5), create_rectangle(15,15)])
+    structure_1 = Structure([create_rectangle(5, 5), create_rectangle(5, 15)])
+    structure_2 = Structure([create_rectangle(15, 5), create_rectangle(15, 15)])
 
     expected_structure = [structure_1.polygons[0].points, structure_2.polygons[1].points]
 
@@ -23,5 +24,4 @@ def test_crossover_true():
         new_structure = crossover(structure_1, structure_2, domain)
         if all([new_structure.polygons[0].points == expected_structure[0],
                 new_structure.polygons[1].points == expected_structure[1]]):
-                assert True
-                
+            assert True
