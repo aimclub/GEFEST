806452db7283a2e9cf1a9bb06c817d71fbd730c3
Denis Sidorenko <sidorenko.den95@gmail.com>
2022-04-26 11:58:51 +0300

    added test where crossover will not be passed

--- test/test_crossover.py
+++ test/test_crossover.py
@@ -6,17 +6,18 @@ from gefest.core.structure.structure import Structure
 from gefest.core.structure.domain import Domain
 
 
+domain = Domain()
+
 def create_rectangle(x, y):
     rectangle_points = [(x, y), (x, y+5), (x+5, y+5), (x+5, y), (x, y)]
     rectangle_poly = Polygon(f'rectangle from {x,y}', points=[Point(*coords) for coords in rectangle_points])
     return rectangle_poly
 
+structure_1 = Structure([create_rectangle(5, 5), create_rectangle(5, 15)])
+structure_2 = Structure([create_rectangle(15, 5), create_rectangle(15, 15)])
+
 
 def test_crossover_true():
-
-    domain = Domain()
-    structure_1 = Structure([create_rectangle(5, 5), create_rectangle(5, 15)])
-    structure_2 = Structure([create_rectangle(15, 5), create_rectangle(15, 15)])
 
     expected_structure = [structure_1.polygons[0].points, structure_2.polygons[1].points]
 
@@ -25,3 +26,11 @@ def test_crossover_true():
         if all([new_structure.polygons[0].points == expected_structure[0],
                 new_structure.polygons[1].points == expected_structure[1]]):
             assert True
+
+
+
+def test_crossover_false():
+
+    new_structure = crossover(structure_1, structure_2, domain, rate=0.001)
+
+    assert any([new_structure==structure_1, new_structure==structure_2])
