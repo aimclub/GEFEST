fb80830f3f81603f04f0e6eef711ff55141ae1d7
Denis Sidorenko <sidorenko.den95@gmail.com>
2022-07-06 13:31:50 +0300

    point doc stg33

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -62,7 +62,7 @@ html_theme = "sphinx_rtd_theme"
 
 # -- Extension configuration -------------------------------------------------
 
-autodoc_inherit_docstrings = False
+# autodoc_inherit_docstrings = False
 # napoleon_google_docstring = True
 # napoleon_include_init_with_doc = True
 # napoleon_numpy_docstring = True
