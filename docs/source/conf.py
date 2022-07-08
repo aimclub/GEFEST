92f99ce2f4fa54340b69501368fd953fea449dd3
Denis Sidorenko <sidorenko.den95@gmail.com>
2022-07-08 12:11:24 +0300

    finding napoleon settings for better strucuture view stg2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,8 +67,22 @@ html_theme = "sphinx_rtd_theme"
 # autodoc_inherit_docstrings = False
 # napoleon_google_docstring = True
 # napoleon_include_init_with_doc = True
-# napoleon_numpy_docstring = True
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = True
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = True
+napoleon_use_admonition_for_notes = True
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = True
+napoleon_use_keyword = True
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
 napoleon_attr_annotations = False
+
 autodoc_default_options = {
     'members': True,
     'undoc-members': False,
