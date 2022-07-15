77bf89e64ad3b327f7453da8a504116b3f8ed52d
quickjkee <jkee2013@mail.ru>
2022-07-15 17:04:35 +0300

    path to models added

--- cases/heat/configuration_dl/heat_estimator.py
+++ cases/heat/configuration_dl/heat_estimator.py
@@ -1,13 +1,19 @@
+from pathlib import Path
+
 from gefest.tools.estimators.DL.heat.heat_cnn import CNN
 from gefest.tools.estimators.estimator import Estimator
 
 
-def configurate_estimator():
+def configurate_estimator(path_to_cnn=False):
     # ------------
     # User-defined estimator
     # it should be created as object with .estimate() method
     # ------------
-    cnn = CNN()
+    if not path_to_cnn:
+        root_path = Path(__file__).parent.parent.parent.parent
+        path_to_cnn = f'{root_path}/gefest/tools/estimators/DL/heat/effnet_mean'
+
+    cnn = CNN(path_to_cnn)
 
     # ------------
     # GEFEST estimator
--- cases/heat/main.py
+++ cases/heat/main.py
@@ -5,21 +5,23 @@ from gefest.core.opt.gen_design import design
 from cases.heat.configuration_dl import heat_sampler, heat_estimator
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--pop_size", type=int, default=5, help='number of individs in population')
+parser.add_argument("--pop_size", type=int, default=1, help='number of individs in population')
 parser.add_argument("--n_steps", type=int, default=10000, help='number of generative design steps')
 parser.add_argument('--n_polys', type=int, default=5, help='maximum number of polygons in structure')
 parser.add_argument('--n_points', type=int, default=15, help='maximum number of points in polygon')
 parser.add_argument('--c_rate', type=float, default=0.6, help='crossover rate')
 parser.add_argument('--m_rate', type=float, default=0.6, help='mutation rate')
 parser.add_argument('--is_closed', type=bool, default=False, help='type of polygon')
+parser.add_argument('--path_to_sampler', type=str, default=False, help='path to deep learning sampler')
+parser.add_argument('--path_to_estimator', type=str, default=False, help='path to deep learning estimator')
 opt = parser.parse_args()
 
 # ------------
 # GEFEST tools configuration
 # ------------
 
-estimator = heat_estimator.configurate_estimator()
-sampler = heat_sampler.configurate_sampler(None)
+estimator = heat_estimator.configurate_estimator(path_to_cnn=opt.path_to_estimator)
+sampler = heat_sampler.configurate_sampler(domain=None, path_to_sampler=opt.path_to_sampler)
 optimizer = None
 
 # ------------
