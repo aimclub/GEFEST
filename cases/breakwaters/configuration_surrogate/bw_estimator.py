6ce6d4fb125b21fa3475586a0f682c8b62cb44e9
quickjkee <jkee2013@mail.ru>
2022-07-15 03:00:54 +0300

    path to model added

--- cases/breakwaters/configuration_surrogate/bw_estimator.py
+++ cases/breakwaters/configuration_surrogate/bw_estimator.py
@@ -8,20 +8,26 @@ from gefest.tools.estimators.estimator import Estimator
 import cases.breakwaters.configuration_de.bw_domain as area
 
 
-def configurate_estimator(domain, path=False):
+def configurate_estimator(domain, path_sim=False, path_sur=False):
     # ------------
     # User-defined estimator
     # it should be created as object with .estimate() method
     # ------------
-    if not path:
+    if not path_sim:
         root_path = Path(__file__).parent.parent.parent.parent
-        path = f'{root_path}/gefest/tools/estimators/simulators/swan/swan_model/'
+        path_sim = f'{root_path}/gefest/tools/estimators/simulators/swan/swan_model/'
 
-    swan = Swan(path=path,
+    if not path_sur:
+        root_path = Path(__file__).parent.parent.parent.parent
+        path_sur = f'{root_path}/gefest/tools/estimators/DL/bw_surrogate/bw_surrogate_700_train.h5'
+
+    swan = Swan(path=path_sim,
                 targets=area.targets,
                 grid=area.grid,
                 domain=domain)
+
     cnn = CNN(domain=domain,
+              path=path_sur,
               main_model=swan)
 
     # Multi-criteria loss function, in our case - HW and cost of structure
