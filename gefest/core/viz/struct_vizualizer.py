9b93f7f69bad1be01554274e1a374f6c50782f56
Denis Sidorenko <sidorenko.den95@gmail.com>
2022-07-21 15:59:27 +0300

    added pics to docs stg7

--- gefest/core/viz/struct_vizualizer.py
+++ gefest/core/viz/struct_vizualizer.py
@@ -29,11 +29,14 @@ class StructVizualizer:
             >>> from gefest.core.structure.structure import get_random_structure
             >>> struct = get_random_structure(domain)
             >>> viz.plot_structure(struct)
+            |viz|
 
         .. image:: https://i.ibb.co/r0YsVtR/vizualizer.png
 
         Returns:
             plt.plot: visualization of the :obj:`Structure`
+
+        .. |viz| image:: https://i.ibb.co/r0YsVtR/vizualizer.png
         """
 
         for poly in struct.polygons:
