411a59feece1742c86f8876fc77deff519793697
Denis S <sidorenko.den95@gmail.com>
2023-06-26 17:40:03 +0300

    added unit-test for the sound sim

---
+++ test/test_sound_simulator.py
@@ -0,0 +1,37 @@
+import sys
+sys.path.append('C:/Users/user2/GEFEST')
+import pytest
+import pickle
+import numpy as np
+
+from gefest.tools.estimators.simulators.sound_wave.sound_interface import SoundSimulator
+from cases.sound_waves.configuration import sound_domain
+
+def upload_file(path: str):
+    with open(path, "rb") as f:
+        file = pickle.load(f)
+        f.close()
+    return file
+
+domain, _ = sound_domain.configurate_domain(
+    poly_num=1,
+    points_num=30,
+    is_closed=True,
+)
+
+structure_path = "test/files/standart_structure.pickle"
+spl_path = "test/files/standart_spl.pickle"
+
+standart_spl = upload_file(spl_path)
+standart_structure = upload_file(structure_path)
+
+sound = SoundSimulator(domain)
+
+
+def test_sumulator():
+    current_spl = sound.estimate(standart_structure)
+
+    assert (current_spl == standart_spl).all()
+
+if __name__ == "__main__":
+    test_sumulator()
