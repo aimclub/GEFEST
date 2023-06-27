d30d528874df5be13b27f590cf5f73d2c1a0604f
Denis S <sidorenko.den95@gmail.com>
2023-06-27 15:48:05 +0300

    typing error fixed

--- gefest/core/structure/prohibited.py
+++ gefest/core/structure/prohibited.py
@@ -1,25 +1,25 @@
 from shapely.geometry.point import Point
-from typing import Optional
+from typing import Optional, List
 
 from gefest.core.structure.structure import Structure
 from gefest.core.structure.structure import Polygon
 from gefest.core.structure.structure import Point as G_Point
 
 
-def create_prohibited(targets: Optional[list[list]] = None, fixed_points: Optional[list[list]] = None,
-                      fixed_area: Optional[list[list]] = None) -> Structure:
+def create_prohibited(targets: Optional[List[List]] = None, fixed_points: Optional[List[List]] = None,
+                      fixed_area: Optional[List[List]] = None) -> Structure:
     """
     Creation of fixed, prohibited structures. Polygons cannot cross them
 
-    :param targets: (Optional[list[list]]), fixed targets inside domain
-    :param fixed_points: (Optional[list[list]]), fixed lines inside domain
-    :param fixed_area: (Optional[list[list]]), fixed areas inside domain
+    :param targets: (Optional[List[List]]), fixed targets inside domain
+    :param fixed_points: (Optional[List[List]]), fixed lines inside domain
+    :param fixed_area: (Optional[List[List]]), fixed areas inside domain
     :return: Structure, structure of all prohibited polygons (targets, lines, areas)
     ::TODO:: change buffer to something more interpretable
     """
     prohibited_area = []
     if targets is not None:
-        target_polygons = [list(Point(target).buffer(20).exterior.coords) for target in targets]
+        target_polygons = [List(Point(target).buffer(20).exterior.coords) for target in targets]
         target_points = [[G_Point(p[0], p[1]) for p in target] for target in target_polygons]
         poly_targets = [Polygon(polygon_id='prohibited_target', points=points) for points in target_points]
         prohibited_area += poly_targets
