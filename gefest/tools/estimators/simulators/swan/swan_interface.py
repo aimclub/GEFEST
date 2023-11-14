3d34220139d6e5c1a4de2513859161e54149f930
Gleb Solovev <glebsolo46@gmail.com>
2023-11-14 20:08:21 +0300

    Fix flake8 errors

--- gefest/tools/estimators/simulators/swan/swan_interface.py
+++ gefest/tools/estimators/simulators/swan/swan_interface.py
@@ -9,6 +9,7 @@ from gefest.tools import Estimator
 
 
 class Swan(Estimator):
+    """Class for SWAN estimator."""
     def __init__(
         self,
         path,
@@ -25,22 +26,28 @@ class Swan(Estimator):
         self.grid = grid
         self.domain = domain
 
-    def estimate(self, struct: Structure):
+    def estimate(self, struct: Structure) -> float:
+        """Function to estimate wave high.
+
+        :param struct: Structure with polygons
+
+        :return: metric of wave high
+        """
         polygons = struct.polygons
 
         file_toread = self.path_to_input + "_2"
         with open(file_toread, "r") as file_to_read:
             content_read = file_to_read.read()
             file_to_read.close()
-            for j, poly in enumerate(polygons):
+            for _j, poly in enumerate(polygons):
                 for_input = "\nOBSTACLE TRANSM 0. REFL 0. LINE "
-                num_of_points = len(2 * poly.points)
                 points = np.array([p.coords[:2] for p in poly.points])
                 individ = points.reshape(-1)
                 for i, gen in enumerate(individ):
                     for_input += "{:.6f}".format(gen / 500)
                     if i != len(individ) - 1:
                         for_input += ", "
+
             for_input += "\n$optline"
             content_to_replace = for_input
             content_write = content_read.replace(
@@ -51,6 +58,7 @@ class Swan(Estimator):
                 ],
                 content_to_replace,
             )
+
         input_created = Path(self.path_to_input)
         input_created.touch(exist_ok=True)
         with open(self.path_to_input, "w") as file_to_write:
@@ -74,7 +82,9 @@ class Swan(Estimator):
                     for target in self.targets
                 ]
             )
+
             res.append(hs_target)
+
         hs_target = sum(res) / len(res)
         print("hs_target", hs_target)
         return hs_target
